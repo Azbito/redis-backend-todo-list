31ad17a9a97e35f52faa25cdb770af22d3bff159
azbito <azbitomsg@proton.me>
2024-11-15 22:50:58 -0300

    refactor default port value

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,11 @@ import { app } from "./app";
 dotenv.config();
 
 const start = async () => {
+  const port = Number(envConfig.PORT) || 3000;
+
   try {
-    await app.listen({ port: Number(envConfig.PORT) });
-    app.log.info(`Server running on port ${envConfig.PORT}`);
+    await app.listen({ port });
+    app.log.info(`Server running on port ${port}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
